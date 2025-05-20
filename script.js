6e79a5a59eddac9d404f92c9066a4402019e7d38
ahmadruyani98 <harime212@gmail.com>
2025-05-20 11:44:01 +0700

    Menambahkan script.js

--- script.js
+++ script.js
@@ -1,47 +1,97 @@
 let keranjang = [];
 let totalHarga = 0;
 
-function tambahKeKeranjang(namaKue, harga) {
- keranjang.push({ nama: namaKue, harga: harga });
- totalHarga += harga;
- updateKeranjang();
+function tambahKeKeranjang(namaKue) {
+    const itemKeranjang = keranjang.find(item => item.nama === namaKue);
+
+    if (itemKeranjang) {
+        itemKeranjang.jumlah++;
+    } else {
+        keranjang.push({
+            nama: namaKue,
+            harga: getHargaKue(namaKue), // Ambil harga dari fungsi getHargaKue
+            jumlah: 1
+        });
+    }
+
+    updateKeranjang();
+}
+
+function kurangiJumlah(namaKue) {
+    const itemKeranjang = keranjang.find(item => item.nama === namaKue);
+
+    if (itemKeranjang) {
+        itemKeranjang.jumlah--;
+        if (itemKeranjang.jumlah <= 0) {
+            // Hapus item dari keranjang jika jumlahnya 0 atau kurang
+            keranjang = keranjang.filter(item => item.nama !== namaKue);
+        }
+        updateKeranjang();
+    }
+}
+
+function getHargaKue(namaKue) {
+    // Fungsi untuk mendapatkan harga kue berdasarkan nama (ganti dengan data yang sesuai)
+    switch (namaKue) {
+        case 'Kue Coklat':
+            return 25000;
+        case 'Kue Vanila':
+            return 20000;
+        case 'Kue Strawberry':
+            return 30000;
+        default:
+            return 0;
+    }
 }
 
 function updateKeranjang() {
- const keranjangBelanja = document.getElementById('keranjang-belanja');
- keranjangBelanja.innerHTML = ''; // Kosongkan isi keranjang
+    const keranjangBelanja = document.getElementById('keranjang-belanja');
+    const totalHargaSpan = document.getElementById('total-harga');
 
- if (keranjang.length === 0) {
- keranjangBelanja.innerHTML = '<li>Keranjang belanja masih kosong.</li>';
- } else {
- keranjang.forEach(item => {
- const listItem = document.createElement('li');
- listItem.textContent = item.nama + ' - Rp ' + item.harga;
- keranjangBelanja.appendChild(listItem);
- });
- }
+    // Kosongkan keranjang belanja
+    keranjangBelanja.innerHTML = '';
 
- document.getElementById('total-harga').textContent = totalHarga;
+    // Reset total harga
+    totalHarga = 0;
+
+    if (keranjang.length === 0) {
+        keranjangBelanja.innerHTML = '<li>Keranjang belanja masih kosong.</li>';
+    } else {
+        keranjang.forEach(item => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `${item.nama} - Rp ${item.harga.toLocaleString()} x ${item.jumlah}
+                                    <br>
+                                    onclick="kurangiJumlah('${item.nama}')">-</button>
+                                    <span>${item.jumlah}</span>
+                                    <button onclick="tambahJumlah('${item.nama}')">+</button>`;
+            keranjangBelanja.appendChild(listItem);
+            totalHarga += item.harga * item.jumlah;
+        });
+    }
+
+    totalHargaSpan.textContent = totalHarga.toLocaleString();
 }
 
 function checkout() {
- if (keranjang.length === 0) {
- alert('Keranjang belanja Anda masih kosong!');
- return;
- }
+    if (keranjang.length === 0) {
+        alert('Keranjang belanja Anda masih kosong!');
+        return;
+    }
 
- let pesan = "Hai, saya ingin memesan:\n";
- keranjang.forEach(item => {
- pesan += "- " + item.nama + " - Rp " + item.harga + "\n";
- });
- pesan += "Total: Rp " + totalHarga;
+    let pesan = "Hai, saya ingin memesan:
+";
+    keranjang.forEach(item => {
+        pesan += "- " + item.nama + " - Rp " + item.harga + " x " + item.jumlah + "
+";
+    });
+    pesan += "Total: Rp " + totalHarga.toLocaleString();
 
- const nomorWhatsApp = "+6282268487849";
- const tautanWhatsApp = "https://wa.me/" + nomorWhatsApp + "?text=" + encodeURIComponent(pesan);
+    const nomorWhatsApp = "+6282268487849";
+    const tautanWhatsApp = "https://wa.me/" + nomorWhatsApp + "?text=" + encodeURIComponent(pesan);
 
- window.open(tautanWhatsApp, '_blank');
+    window.open(tautanWhatsApp, '_blank');
 
- keranjang = [];
- totalHarga = 0;
- updateKeranjang();
+    keranjang = [];
+    totalHarga = 0;
+    updateKeranjang();
 }
